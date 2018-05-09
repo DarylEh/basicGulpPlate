bfa019ca9523cb42106965ccbb7aa379e10e5e10
Daryl Amodan <darylamodan@gmail.com>
2018-05-09 11:36:56 -0400

    emptied dist folder

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Remember to do run the `npm install` command after cloning this repo :)
+
+Type `gulp` to run
+
+Type `gulp build` to add dist folder
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var btn = document.querySelector(".btn");
-
+console.log("test");
 btn.addEventListener("click", function () {
-  alert("hello world");
+  console.log("test");
 });
